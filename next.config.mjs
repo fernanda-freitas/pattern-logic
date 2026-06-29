3ce8c5f2c84f12f7e224b9c69872b8cac7e18129
Fernanda Freitas <idea.fernanda@gmail.com>
2026-06-29 16:06:55 +0100

    no message

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/pattern-logic',
+  basePath: isProd ? '/pattern-logic' : '',
 };
 
 export default nextConfig;
